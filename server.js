cfeaae0fe5cefbb86f30abce7042e86c9bc80497
Jamie Woodmancy <jamie29w@gmail.com>
2018-10-04 12:14:12 -0700

    added moment and timestamp; changed formatting so newest ticks are at top; general code reformatting

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3200
 const axios = require('axios')
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://api2.poloniex.com');
+const moment = require('moment')
+const WebSocket = require('ws')
+const ws = new WebSocket('wss://api2.poloniex.com')
 const wss = new WebSocket.Server({
     port: 8080
 })
@@ -14,38 +15,42 @@ const socketSubscription = {
 }
 
 ws.on('open', function open() {
-    ws.send(JSON.stringify(socketSubscription));
-});
+    ws.send(JSON.stringify(socketSubscription))
+})
 
 wss.broadcast = function (data) {
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(data)
         }
-    });
-}
-
-const getUsdPrice = (lastEthPrice) => {
-    axios.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/").then(res => {
-        const priceUsd = res.data[0].price_usd
-        let output = {
-            lastEthPrice,
-            priceUsd
-        }
-        wss.broadcast(JSON.stringify(output))
     })
 }
 
-ws.on('message', function incoming(data) {
+const getUsdPrice = async (lastEthPrice) => {
+    const res = await axios.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
+    const priceUsd = res.data[0].price_usd
+    return {
+        lastEthPrice,
+        priceUsd
+    }
+}
+
+ws.on('message', async function incoming(data) {
     const message = JSON.parse(data)
     if (message[1] === null) {
         const channel = message[2][0]
         if (channel === 148) {
             const lastEthPrice = message[2][1]
-            getUsdPrice(lastEthPrice)
+            const momentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+            let coinDataOutput = await getUsdPrice(lastEthPrice)
+            output = {
+                ...coinDataOutput,
+                momentTime
+            }
+            wss.broadcast(JSON.stringify(output))
         }
     }
-});
+})
 
 app.use('/', express.static('client'))
 
