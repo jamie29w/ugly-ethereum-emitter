0675ae77e56c295402279af678c41f45ace7384b
Jamie Woodmancy <jamie29w@gmail.com>
2018-10-04 02:37:18 -0700

    initial commit

---
+++ client/app.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    const messagesList = document.getElementById('messagesList')
+    const socketStatus = document.getElementById('socketStatus')
+    const btn = document.getElementById('btn')
+  
+    const socket = new WebSocket('ws://localhost:8080')
+  
+    socket.onopen = (e) => {
+        socketStatus.innerHTML = `Connected to: ${ e.currentTarget.url }`
+    }
+  
+    socket.onerror = (err) => {
+      console.log('WebSocket Error: ', err)
+    }
+
+    const currencyRounder = num => {
+        return Math.round(num * 1e2) / 1e2
+    }
+
+    socket.onmessage = (e) => {
+        let data = JSON.parse(e.data)
+        messagesList.innerHTML += `<li class="received"><span>Last Ethereum Trade Price: $${ currencyRounder(data.lastEthPrice) }</span> <span>Converted USD is $${ currencyRounder(data.priceUsd * data.lastEthPrice) }</span></li>`
+    }
+  
+    socket.onclose = (e) => {
+      socketStatus.innerHTML = "Disconnected from WebSocket."
+    }
+  
+    btn.onclick = (e) => {
+        e.preventDefault()  
+        socket.close()
+    }
+  }
+  
---
+++ server.js
@@ -0,0 +1,44 @@
+const app = require('express')()
+const port = 3200
+const axios = require('axios')
+const WebSocket = require('ws');
+const ws = new WebSocket('wss://api2.poloniex.com');
+const wss = new WebSocket.Server({ port: 8080 })
+
+const socketSubscription = {
+    "command": "subscribe",
+    "channel": 1002
+}
+
+ws.on('open', function open() {
+  ws.send(JSON.stringify(socketSubscription));
+});
+
+wss.broadcast = function (data) {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data)
+        }
+    });
+}
+
+const getUsdPrice = (lastEthPrice) => {
+    axios.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/").then(res => {
+    const priceUsd = res.data[0].price_usd
+    let output = {lastEthPrice, priceUsd}
+    wss.broadcast(JSON.stringify(output))
+    })
+}
+ 
+ws.on('message', function incoming(data) {
+      const message = JSON.parse(data)
+      if (message[1] === null) {
+        const channel = message[2][0]
+        if (channel === 148) {
+            const lastEthPrice = message[2][1]
+            getUsdPrice(lastEthPrice)
+        }
+      }
+});
+
+app.listen(port, () => {console.log(`listening on ${port}`)})
